36ead4a3a42201af46489920a937d0bf68d9e48b
Nicole Zoppi <nicolezoppi1999@gmail.com>
2023-02-25 12:11:47 +0000

    Added method 4, added on method 2 and 3 the argument self

--- class.py
+++ class.py
@@ -71,7 +71,7 @@ class Group22:
         return df
     
     
-    def get_countries(df):
+    def get_countries(self, df):
         """
         Returns a list of all the countries of the dataset
 
@@ -86,7 +86,7 @@ class Group22:
         """
         return list(df['Entity'].unique())
     
-    def plot_quantity_correlation(df):
+    def plot_quantity_correlation(self, df):
         """
         Returns a correlation matrix of the quantity columns
 
@@ -105,4 +105,43 @@ class Group22:
 
         sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
         plt.title('Correlation Matrix of Quantity Columns')
+        plt.show()
+        
+    def plot_output_area_chart(self, df, country=None, normalize=False):
+        """
+        Plots an area chart of the distinct "_output_" columns.
+
+        Parameters
+        ----------
+        df : pandas dataframe
+            dataframe of the dataset
+        country : str or None
+            Country name. If None or 'World', plots the sum for all countries.
+        normalize : bool
+            If True, normalizes the output in relative terms: each year, output should always be 100%.
+
+        Returns
+        -------
+        area chart of the distinct "_output_" columns
+        """
+        output_cols = [col for col in self.df.columns if '_output_' in col]
+        if not output_cols:
+            raise ValueError("No output columns found in the dataset.")
+
+        if country is None or country == 'World':
+            df_country = self.df.groupby('Year')[output_cols].sum().reset_index()
+            title = 'World Output'
+        else:
+            if country not in self.get_countries(df):
+                raise ValueError(f"{country} is not a valid country name.")
+            df_country = self.df[self.df['Entity'] == country][['Year'] + output_cols]
+            title = f"{country} Output"
+
+        if normalize:
+            df_country[output_cols] = df_country[output_cols].div(df_country[output_cols].sum(axis=1), axis=0) * 100
+
+        df_country.set_index('Year').plot(kind='area', stacked=True)
+        plt.title(title)
+        plt.xlabel('Year')
+        plt.ylabel('Output')
         plt.show()
